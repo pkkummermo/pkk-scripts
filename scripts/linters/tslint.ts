0cd1a2a23613a6d665843e1a09a7833ec9cb8862
Per Kristian Kummermo <per.kristian@kummermo.no>
2019-01-22 22:52:34 +0100

    refactor(tslint-vars): remove explicit exclude as default

--- scripts/linters/tslint.ts
+++ scripts/linters/tslint.ts
@@ -2,7 +2,7 @@ import path from "path";
 
 export const TS_LINT_VARS = {
     DEFAULT_EXCLUDES: ["node_modules/**"],
-    DEFAULT_ARGS: ["--project", ".", "--exclude", path.normalize(`node_modules/**`)],
+    DEFAULT_ARGS: ["--project", "."],
     FALLBACK_CONFIG: ["-c", path.join(__dirname, "./configs/tslint.json").replace(process.cwd(), ".")],
     CONFIG_FILES: ["tslint.json", "tslint.yml"],
 };
