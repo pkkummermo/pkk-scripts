f1bcd5c922bff04f6bf3f28eb139c775ef91c356
Per Kristian Kummermo <per.kristian@kummermo.no>
2019-02-02 20:27:59 +0100

    fix(tslint): fix reversed bool when checking for tsconfig

--- scripts/lint.ts
+++ scripts/lint.ts
@@ -87,7 +87,7 @@ const lintJavaScript = async (args: string[]) => {
 
 export const lintScript = async (args: string[] = [], lintArgs: ILintCommand) => {
     const spinner = ora();
-    const hasTSConfig = !hasOneOfFiles(["tsconfig.json"]);
+    const hasTSConfig = hasOneOfFiles(["tsconfig.json"]);
 
     const shouldLintTS = shouldLint(lintArgs, ["ts", "typescript"]) && hasTSConfig;
     const shouldLintJS = shouldLint(lintArgs, ["js", "javascript"]);
