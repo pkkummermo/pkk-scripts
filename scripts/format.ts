a57620dce4ecce42f4ed5d7b52513e63dabb06fd
Per Kristian Kummermo <per.kristian@kummermo.no>
2019-02-03 21:25:17 +0100

    fix(format): use cross-spawn due to windows

--- scripts/format.ts
+++ scripts/format.ts
@@ -1,4 +1,4 @@
-import { spawn } from "child_process";
+import spawn from "cross-spawn";
 import { hasOneOfFiles, hasPackageProperty, LOG, resolveBin, spawnProcessPromise } from "../util";
 import { PRETTIER_VARS } from "./formatters/prettier";
 
