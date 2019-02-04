2b6b95293f3b8ffcccb25a3c992ad81d46d93b02
Per Kristian Kummermo <per.kristian@telenordigital.com>
2019-02-04 09:16:44 +0100

    chore(index): prepare version bump

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ LOG("Process args", process.argv);
 const commandArgs = process.argv.slice(3);
 
 commander
-    .version("0.1.9")
+    .version("0.1.10")
     .name("pkk-scripts")
     .description("pkk-scripts - bootstrapper for projects");
 
